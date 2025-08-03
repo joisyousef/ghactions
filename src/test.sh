fa1fa21c24901a72ffa617ac2811853cbb53b4f1
Yousef Elsrogy <yousefelsrogy500@gmail.com>
2025-08-03 03:21:33 +0300

    fix test.sh file

--- src/test.sh
+++ src/test.sh
@@ -3,7 +3,7 @@
 # src/test.sh
 EXPECTED="Hello, Test!"
 
-OUTPUT=$(node -e "console.log(require('./app').greet('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
   echo "Test passed!"
